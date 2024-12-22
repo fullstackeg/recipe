19b0004fd8c23cab28e5c4659ecaf150c746b656
ANOUARLAACHIR <become.engineer88@gmail.com>
2024-12-22 20:09:22 +0100

    adding HomeController with @RestController to test the app

---
+++ src/main/java/com/fullstack/recipe/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.fullstack.recipe.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String homeController() {
+        return "Hello World";
+    }
+}
